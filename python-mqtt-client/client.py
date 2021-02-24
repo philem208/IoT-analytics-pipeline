e93eabc270a2708cc3e911f0c443b594424b364f
Philip Empl <philip.empl@wiwi.uni-regensburg.de>
2021-02-24 19:55:35 +0100

    update wf action

--- python-mqtt-client/client.py
+++ python-mqtt-client/client.py
@@ -9,20 +9,34 @@ topic =  "LENZDRGB611"
 msg_string = '{"temp": 20, "location": "LENZDRGB611", "type": "temperature", "units":  {"temp": "° C"}}'
 msg_json = json.loads(msg_string)
 
-client = mqtt.Client()
-client.connect(broker, port, 60)
+#Connection success callback
+def on_connect(client, userdata, flags, rc):
+    print('Connected with result code '+str(rc))
+    client.subscribe('testtopic/#')
 
-def publish(client):
+# Message receiving callback
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+# send random temperature values to mqtt topic
+def publish(client):    
      while True:
          time.sleep(random.randint(1, 10))
          msg_json["temp"] = round(random.uniform(20, 30), 2)
-         result = client.publish(topic, json.dumps(msg_json))
+         msg_payload = json.dumps(msg_json)
+         result = client.publish(topic, payload="test",qos=0, retain=False)
          # result: [0, 1]
          status = result[0]
          if status == 0:
              print(f"Send `{msg_json}` to topic `{topic}`")
          else:
              print(f"Failed to send message to topic {topic}")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker, port, 60)
+
+
          
 publish(client)
 client.loop_forever()
